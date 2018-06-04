62b3671357746b317c4920ea194060034ff00c6c
Danilo Albuquerque <danilospalbuquerque@gmail.com>
2018-06-03 21:19:57 -0300

    Improve performance removing repeated recursions

--- file.rb
+++ file.rb
@@ -7,33 +7,37 @@ def not_in_region?(current_region_elements, i, j)
   current_region_elements.detect { |e| e[0] == i and e[1] == j }.nil?
 end
 
-def connected_cells_count(matrix, i, j, current_region_elements = [])
-  return 0 if j == matrix[0].size or i == -1
-  return 0 if i == matrix.size or j == -1
-  return 0 if matrix[i][j].zero?
-  return 0 unless not_in_region?(current_region_elements, i, j)
+def connect_cells(matrix, i, j, current_region_elements = [])
+  return if j == matrix[0].size or i == -1
+  return if i == matrix.size or j == -1
+  return if matrix[i][j].zero?
+  return unless not_in_region?(current_region_elements, i, j)
 
-  count = 1
   current_region_elements << [i, j]
-  count += connected_cells_count(matrix, i, j + 1, current_region_elements)
-  count += connected_cells_count(matrix, i + 1, j + 1, current_region_elements)
-  count += connected_cells_count(matrix, i + 1, j, current_region_elements)
-  count += connected_cells_count(matrix, i + 1, j - 1, current_region_elements)
-  count += connected_cells_count(matrix, i, j - 1, current_region_elements)
-  count += connected_cells_count(matrix, i - 1, j - 1, current_region_elements)
-  count += connected_cells_count(matrix, i - 1, j, current_region_elements)
-  count += connected_cells_count(matrix, i - 1, j + 1, current_region_elements)
+  connect_cells(matrix, i, j + 1, current_region_elements)
+  connect_cells(matrix, i + 1, j + 1, current_region_elements)
+  connect_cells(matrix, i + 1, j, current_region_elements)
+  connect_cells(matrix, i + 1, j - 1, current_region_elements)
+  connect_cells(matrix, i, j - 1, current_region_elements)
+  connect_cells(matrix, i - 1, j - 1, current_region_elements)
+  connect_cells(matrix, i - 1, j, current_region_elements)
+  connect_cells(matrix, i - 1, j + 1, current_region_elements)
 end
 
 def connected_cell(matrix)
-  results = []
+  scanned_elements = []
+  max = 0
   matrix.size.times.each do |i|
     matrix[i].size.times.each do |j|
-      results << connected_cells_count(matrix, i, j)
+      next unless not_in_region?(scanned_elements, i, j)
+      region_elements = []
+      connect_cells(matrix, i, j, region_elements)
+      scanned_elements.concat(region_elements)
+      max = region_elements.size if region_elements.size > max
     end
   end
 
-  results.max
+  max
 end
 
 fptr = File.open(ENV['OUTPUT_PATH'], 'w')
