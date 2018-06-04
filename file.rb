debe3f6589f7412ee0844d063e0d6311faa87168
Danilo Albuquerque <danilospalbuquerque@gmail.com>
2018-06-03 22:37:29 -0300

    Reduce algorithm complexity using next element pointer

--- file.rb
+++ file.rb
@@ -3,18 +3,37 @@
 require 'json'
 require 'stringio'
 
-def connected_cells_count(matrix, i, j, scanned_elements)
+def redefine_next(next_element, scanned_elements)
+  i, j = next_element
+  n = scanned_elements.size
+  m = scanned_elements[0].size
+
+  next_i = i + ((j + 1) / m)
+  next_j = (j + 1) % m
+
+  next_element[0] = next_i
+  next_element[1] = next_j
+
+  return if next_i == n
+  return unless scanned_elements[next_i][next_j]
+  redefine_next(next_element, scanned_elements)
+end
+
+def connected_cells_count(matrix, i, j, scanned_elements, next_element)
   return 0 if j == matrix[0].size or i == -1
   return 0 if i == matrix.size or j == -1
-  return 0 if matrix[i][j].zero?
   return 0 if scanned_elements[i][j]
 
+  redefine_next(next_element, scanned_elements) if next_element[0] == i and next_element[1] == j
   scanned_elements[i][j] = true
+
+  return 0 if matrix[i][j].zero?
+
   count = 1
   (-1..1).each do |line|
     (-1..1).each do |column|
       next if line == 0 and column == 0
-      count += connected_cells_count(matrix, i + line, j + column, scanned_elements)
+      count += connected_cells_count(matrix, i + line, j + column, scanned_elements, next_element)
     end
   end
 
@@ -24,16 +43,13 @@ end
 def connected_cell(matrix)
   n = matrix.size
   m = matrix[0].size
-  scanned_elements = Array.new(n)
-  n.times { |n| scanned_elements[n]= Array.new(m) }
+  scanned_elements = n.times.map { Array.new(m) }
 
   results = []
-
-  n.times.each do |i|
-    m.times.each do |j|
-      next if scanned_elements[i][j]
-      results << connected_cells_count(matrix, i, j, scanned_elements)
-    end
+  next_element = [0, 0]
+  while next_element[0] != n do
+    i, j = next_element
+    results << connected_cells_count(matrix, i, j, scanned_elements, next_element)
   end
 
   results.max
